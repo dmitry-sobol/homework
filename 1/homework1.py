bf1f656223c6882b22cc625c3e7d32f456820087
Dmitry Sobol <dmitry-sobol@MacBook-Dmitry.local>
2016-09-07 21:11:18 +0300

    homework 1

---
+++ 1/homework1.py
@@ -0,0 +1,27 @@
+__author__ = 'dmitry-sobol'
+
+#vars init
+i = 0
+true_answers = 0
+
+#questions and answers
+question = ['Опишите одним словом вторую версию пайтона?', 
+            'А третью?', 
+            'Какая кодировка используется в python3 по умолчанию?',
+            'Как инициировать git репозиторий?',
+            'Как сделать коммит в git?']
+answer = ['плохая', 
+          'хорошая',
+          'unicode',
+          'git init',
+          'git commit']
+
+while i < len(question):
+    print (question[i], ":")
+    user_answer = input()
+    if user_answer == (answer[i]):
+        true_answers += 1
+        print ("Ответ верный, верных ответов: ", true_answers, "\n")
+    else:
+        print ("Ответ не верный \n")
+    i += 1
